308f870294e5e5df9c4d0c803e64643ce3ddaae2
Elias Koromilas <elias.koromilas@gmail.com>
2021-02-16 18:05:05 +0200

    Implement the docker/kubelet plugins

---
+++ internal/plugins/docker.go
@@ -0,0 +1,215 @@
+package plugins
+
+import (
+	"context"
+	"net"
+	"strings"
+
+	"github.com/docker/go-plugins-helpers/volume"
+	"github.com/inaccel/daemon/internal/driver"
+	"github.com/inaccel/daemon/pkg/plugin"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
+)
+
+type Docker struct {
+	path string
+
+	driver driver.Driver
+	plugin.Plugin
+}
+
+func NewDocker(ctx context.Context, driver driver.Driver) plugin.Plugin {
+	ctx, cancel := context.WithCancel(ctx)
+
+	docker := &Docker{
+		path: "/run/docker/plugins/inaccel.sock",
+	}
+
+	docker.driver = driver
+
+	docker.Plugin = plugin.Base(func() {
+		unix.Unlink(docker.path)
+		if listener, err := net.Listen("unix", docker.path); err == nil {
+			go func() {
+				<-ctx.Done()
+
+				listener.Close()
+			}()
+
+			server := volume.NewHandler(docker)
+
+			server.Serve(listener)
+		} else {
+			logrus.Warn(err)
+		}
+	}, cancel)
+
+	return docker
+}
+
+func (plugin Docker) Capabilities() *volume.CapabilitiesResponse {
+	logrus.Info("Docker/Capabilities")
+
+	response := &volume.CapabilitiesResponse{
+		Capabilities: volume.Capability{
+			Scope: "local",
+		},
+	}
+
+	return response
+}
+
+func (plugin Docker) Create(request *volume.CreateRequest) error {
+	logrus.Info("Docker/Create")
+
+	var name string
+	if !strings.EqualFold(request.Name, "host") {
+		name = request.Name
+	}
+
+	_, err := plugin.driver.Create(name)
+
+	return err
+}
+
+func (plugin Docker) Get(request *volume.GetRequest) (*volume.GetResponse, error) {
+	logrus.Info("Docker/Get")
+
+	response := &volume.GetResponse{}
+
+	if strings.EqualFold(request.Name, "inaccel") {
+		response.Volume = &volume.Volume{
+			Name: request.Name,
+			Status: map[string]interface{}{
+				"version": plugin.driver.Version(),
+			},
+		}
+	} else {
+		var name string
+		if !strings.EqualFold(request.Name, "host") {
+			name = request.Name
+		}
+
+		mountpoint, err := plugin.driver.Get(name)
+		if err != nil {
+			return nil, err
+		}
+
+		response.Volume = &volume.Volume{
+			Name:       request.Name,
+			Mountpoint: mountpoint,
+			Status: map[string]interface{}{
+				"version": plugin.driver.Version(),
+			},
+		}
+	}
+
+	return response, nil
+}
+
+func (plugin Docker) List() (*volume.ListResponse, error) {
+	logrus.Info("Docker/List")
+
+	response := &volume.ListResponse{}
+
+	volumes, err := plugin.driver.List()
+	if err != nil {
+		return nil, err
+	}
+
+	for name, mountpoint := range volumes {
+		response.Volumes = append(response.Volumes, &volume.Volume{
+			Name:       name,
+			Mountpoint: mountpoint,
+			Status: map[string]interface{}{
+				"version": plugin.driver.Version(),
+			},
+		})
+	}
+
+	return response, nil
+}
+
+func (plugin Docker) Mount(request *volume.MountRequest) (*volume.MountResponse, error) {
+	logrus.Info("Docker/Mount")
+
+	response := &volume.MountResponse{}
+
+	if strings.EqualFold(request.Name, "inaccel") {
+		mountpoint, err := plugin.driver.Create(request.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		response.Mountpoint = mountpoint
+	} else {
+		var name string
+		if !strings.EqualFold(request.Name, "host") {
+			name = request.Name
+		}
+
+		mountpoint, err := plugin.driver.Get(name)
+		if err != nil {
+			return nil, err
+		}
+
+		response.Mountpoint = mountpoint
+	}
+
+	return response, nil
+}
+
+func (plugin Docker) Path(request *volume.PathRequest) (*volume.PathResponse, error) {
+	logrus.Info("Docker/Path")
+
+	response := &volume.PathResponse{}
+
+	if !strings.EqualFold(request.Name, "inaccel") {
+		var name string
+		if !strings.EqualFold(request.Name, "host") {
+			name = request.Name
+		}
+
+		mountpoint, err := plugin.driver.Get(name)
+		if err != nil {
+			return nil, err
+		}
+
+		response.Mountpoint = mountpoint
+	}
+
+	return response, nil
+}
+
+func (plugin Docker) Remove(request *volume.RemoveRequest) error {
+	logrus.Info("Docker/Remove")
+
+	var name string
+	if !strings.EqualFold(request.Name, "host") {
+		name = request.Name
+	}
+
+	err := plugin.driver.Release(name)
+
+	return err
+}
+
+func (plugin Docker) Unmount(request *volume.UnmountRequest) error {
+	logrus.Info("Docker/Unmount")
+
+	if strings.EqualFold(request.Name, "inaccel") {
+		err := plugin.driver.Release(request.ID)
+
+		return err
+	}
+
+	var name string
+	if !strings.EqualFold(request.Name, "host") {
+		name = request.Name
+	}
+
+	_, err := plugin.driver.Get(name)
+
+	return err
+}
---
+++ internal/plugins/kubelet.go
@@ -0,0 +1,200 @@
+package plugins
+
+import (
+	"context"
+	"net"
+	"os"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/docker/docker/daemon/graphdriver"
+	"github.com/inaccel/daemon/internal/driver"
+	"github.com/inaccel/daemon/pkg/plugin"
+	"github.com/moby/sys/mount"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"k8s.io/kubelet/pkg/apis/pluginregistration/v1"
+)
+
+type Kubelet struct {
+	path string
+
+	driver driver.Driver
+	plugin.Plugin
+}
+
+func NewKubelet(ctx context.Context, driver driver.Driver) plugin.Plugin {
+	ctx, cancel := context.WithCancel(ctx)
+
+	kubelet := &Kubelet{
+		path: "/var/lib/kubelet/plugins_registry/inaccel.sock",
+	}
+
+	kubelet.driver = driver
+
+	kubelet.Plugin = plugin.Base(func() {
+		unix.Unlink(kubelet.path)
+		if listener, err := net.Listen("unix", kubelet.path); err == nil {
+			go func() {
+				<-ctx.Done()
+
+				listener.Close()
+			}()
+
+			server := grpc.NewServer()
+			csi.RegisterIdentityServer(server, kubelet)
+			csi.RegisterNodeServer(server, kubelet)
+			pluginregistration.RegisterRegistrationServer(server, kubelet)
+
+			server.Serve(listener)
+		} else {
+			logrus.Warn(err)
+		}
+	}, cancel)
+
+	return kubelet
+}
+
+func (plugin Kubelet) GetInfo(ctx context.Context, request *pluginregistration.InfoRequest) (*pluginregistration.PluginInfo, error) {
+	logrus.Info("Kubelet/GetInfo")
+
+	response := &pluginregistration.PluginInfo{
+		Type:     pluginregistration.CSIPlugin,
+		Name:     "inaccel",
+		Endpoint: plugin.path,
+		SupportedVersions: []string{
+			"v1.3.0",
+		},
+	}
+
+	return response, nil
+}
+
+func (plugin Kubelet) GetPluginCapabilities(ctx context.Context, request *csi.GetPluginCapabilitiesRequest) (*csi.GetPluginCapabilitiesResponse, error) {
+	logrus.Info("Kubelet/GetPluginCapabilities")
+
+	response := &csi.GetPluginCapabilitiesResponse{}
+
+	return response, nil
+}
+
+func (plugin Kubelet) GetPluginInfo(ctx context.Context, request *csi.GetPluginInfoRequest) (*csi.GetPluginInfoResponse, error) {
+	logrus.Info("Kubelet/GetPluginInfo")
+
+	response := &csi.GetPluginInfoResponse{
+		Name:          "inaccel",
+		VendorVersion: plugin.driver.Version(),
+	}
+
+	return response, nil
+}
+
+func (plugin Kubelet) NodePublishVolume(ctx context.Context, request *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
+	logrus.Info("Kubelet/NodePublishVolume")
+
+	response := &csi.NodePublishVolumeResponse{}
+
+	mountpoint, err := plugin.driver.Create(request.VolumeId)
+	if err != nil {
+		return nil, err
+	}
+
+	if !graphdriver.NewDefaultChecker().IsMounted(request.TargetPath) {
+		if err := mount.Mount(mountpoint, request.TargetPath, "none", "bind"); err != nil {
+			return nil, err
+		}
+	}
+
+	return response, nil
+}
+
+func (plugin Kubelet) NodeUnpublishVolume(ctx context.Context, request *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
+	logrus.Info("Kubelet/NodeUnpublishVolume")
+
+	response := &csi.NodeUnpublishVolumeResponse{}
+
+	if graphdriver.NewDefaultChecker().IsMounted(request.TargetPath) {
+		if err := mount.Unmount(request.TargetPath); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := plugin.driver.Release(request.VolumeId); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (plugin Kubelet) NodeExpandVolume(ctx context.Context, request *csi.NodeExpandVolumeRequest) (*csi.NodeExpandVolumeResponse, error) {
+	logrus.Info("Kubelet/NodeExpandVolume")
+
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (plugin Kubelet) NodeGetCapabilities(ctx context.Context, request *csi.NodeGetCapabilitiesRequest) (*csi.NodeGetCapabilitiesResponse, error) {
+	logrus.Info("Kubelet/NodeGetCapabilities")
+
+	response := &csi.NodeGetCapabilitiesResponse{}
+
+	return response, nil
+}
+
+func (plugin Kubelet) NodeGetInfo(ctx context.Context, request *csi.NodeGetInfoRequest) (*csi.NodeGetInfoResponse, error) {
+	logrus.Info("Kubelet/NodeGetInfo")
+
+	response := &csi.NodeGetInfoResponse{}
+
+	if nodeName, ok := os.LookupEnv("NODE_NAME"); ok {
+		response.NodeId = nodeName
+	} else {
+		hostname, err := os.Hostname()
+		if err != nil {
+			return nil, err
+		}
+
+		response.NodeId = hostname
+	}
+
+	return response, nil
+}
+
+func (plugin Kubelet) NodeGetVolumeStats(ctx context.Context, request *csi.NodeGetVolumeStatsRequest) (*csi.NodeGetVolumeStatsResponse, error) {
+	logrus.Info("Kubelet/NodeGetVolumeStats")
+
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (plugin Kubelet) NodeStageVolume(ctx context.Context, request *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
+	logrus.Info("Kubelet/NodeStageVolume")
+
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (plugin Kubelet) NodeUnstageVolume(ctx context.Context, request *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
+	logrus.Info("Kubelet/NodeUnstageVolume")
+
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (plugin Kubelet) NotifyRegistrationStatus(ctx context.Context, request *pluginregistration.RegistrationStatus) (*pluginregistration.RegistrationStatusResponse, error) {
+	logrus.Info("Kubelet/NotifyRegistrationStatus")
+
+	response := &pluginregistration.RegistrationStatusResponse{}
+
+	if !request.PluginRegistered {
+		logrus.Error(request.Error)
+	}
+
+	return response, nil
+}
+
+func (plugin Kubelet) Probe(ctx context.Context, request *csi.ProbeRequest) (*csi.ProbeResponse, error) {
+	logrus.Info("Kubelet/Probe")
+
+	response := &csi.ProbeResponse{}
+
+	return response, nil
+}
